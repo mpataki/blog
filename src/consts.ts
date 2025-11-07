98d8ce162441789249213462fa1276b414c87352
Mat Pataki <matpataki@gmail.com>
2025-11-07 17:11:27 -0500

    first pass on blog setup

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Mat';
+export const SITE_DESCRIPTION = 'Thoughts, stories, and ideas by Mat Pataki.';
