e5577d46865712740a99aebe93293ec472926f1c
Mat Pataki <matpataki@gmail.com>
2025-11-07 21:49:59 -0500

    fix: cut it down to basics

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Mat';
+export const SITE_TITLE = 'Mat Pataki - Blog';
 export const SITE_DESCRIPTION = 'Thoughts, stories, and ideas by Mat Pataki.';
